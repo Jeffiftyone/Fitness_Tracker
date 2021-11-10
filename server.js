fb989e7f3fa5f1f5dedf997567c58a2d2e0b2819
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-11-09 21:15:35 -0500

    routes added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const path = require("path");
+
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
@@ -17,6 +19,23 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
 
+
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+
 db.Workout.create({ name: "Workout Tracker" })
   .then(dbWorkout => {
     console.log(dbWorkout);
@@ -25,9 +44,9 @@ db.Workout.create({ name: "Workout Tracker" })
     console.log(message);
   });
 
-app.post("/submit", ({body}, res) => {
+app.post("/api/workouts", ({body}, res) => {
   db.Exercise.create(body)
-    .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+    .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -36,37 +55,55 @@ app.post("/submit", ({body}, res) => {
     });
 });
 
-app.get("/exercise", (req, res) => {
-  db.Book.find({})
-    .then(dbBook => {
-      res.json(dbBook);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+app.get("/api/workouts/range", (req, res) => {
+ 
+   (error, data) => {
+      if (error) {
+          res.send(error);
+      } else {
+          res.json(data);
+      }
+    }
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    let ObjectId=mongoose.Types.ObjectId(req.params.id);
+    db.Workout.findByIdAndUpdate(
+      {ObjectId},
+        {
+            $push: {
+                exercises: [req.body]
+            }
+            
+        }, {
+            upsert: true
+        },
+        (error, data) => {
+            if (error) {
+                console.log(error);
+                res.send(error);
+            } else {
+                console.log(data)
+                res.send(data);
+            }
+        }
+    );
 });
 
-app.get("/stats", (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+app.get("/find/:id", (req, res) => {
+  db.Workout.findOne(
+      {
+          _id: mongoose.Types.ObjectId(req.params.id)
+      },
+      (error, data) => {
+          if (error) {
+              res.send(error);
+          } else {
+              res.send(data);
+          }
+      }
+  );
 });
-
-app.get("/populated", (req, res) => {
-  db.Workout.find({})
-    .populate("exercise")
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
